36c371cbc650d5d935e9e182b9cdc2379eb71f69
Joseph Palazzolo <jpalazz2@gmail.com>
2016-03-17 22:45:10 -0400

    Updated user and profile models TODO: complete the PersonalBike and add it to the user model

--- bikeshare_app/models.py
+++ bikeshare_app/models.py
@@ -1,11 +1,25 @@
+"""Models for the app"""
 from bikeshare_app import db
+from datetime import date
+
+
+class Profile(db.EmbeddedDocument):
+    """Embedded user profile"""
+    height = db.IntField(required=True)
+    grad_year = db.StringField(required=True, default=date.today().year)
+    violations = db.StringField()
+    fav_bikes = db.ListField(db.IntField())
 
 
 class User(db.Document):
     """Model for users"""
-    email = db.StringField(required=True, unique=True)
     name = db.StringField(required=True)
+    email = db.EmailField(required=True, unique=True)
     active = db.BooleanField(required=True, default=True)
+    user_type = db.IntField(required=True, default=0)
+    banned = db.BooleanField(required=True, default=False)
+    profile = db.EmbeddedDocumentField(Profile)
+    #TODO: add the personal bike field
 
 
 class Bike(db.Document):
