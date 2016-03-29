d8448e131975da4cde3520c3d04d3f17d661d99d
Joseph Palazzolo <jpalazz2@Josephs-MacBook-Pro-2.local>
2016-03-29 10:36:59 -0400

    Added bike models

--- bikeshare_app/models.py
+++ bikeshare_app/models.py
@@ -18,9 +18,8 @@ class User(db.Document):
     active = db.BooleanField(required=True, default=True)
     user_type = db.IntField(required=True, default=0)
     banned = db.BooleanField(required=True, default=False)
-    profile = db.EmbeddedDocumentField(Profile)
-    # TODO: add the personal bike field
-
+    profile = db.EmbeddedDocumentField(Profile, required=False)
+    bike = db.EmbeddedDocumentField(Personal_Bike, required=False)
 
 class Bike(db.EmbeddedDocument):
     """Model for bikes"""
@@ -32,5 +31,24 @@ class Bike(db.EmbeddedDocument):
     size = db.IntField(required=True)
     repair_active = db.BooleanField(required=True, default=False)
     location = db.StringField(required=True)
-    repair_ids = db.ListField(db.ObjectIdField(), required=True)
+    repair_ids = db.ListField(db.ObjectIdField(), required=False)
     reported_missing = db.BooleanField(required=True, default=False)
+
+
+class Personal_Bike(db.Document):
+    """Model for a users personal bike"""
+    bike = db.EmbeddedDocumentField(Bike, required=True)
+    user = db.ObjectIdField(required=True)
+    owned_since = db.DateTimeField(required=True)
+    comment = db.StringField(required=True)
+    lock_brand = db.StringField(required=False)
+
+
+class Active_Share(db.Document):
+    """Model for the bike share owned bikes"""
+    available = db.BooleanField(required=True, default=False)
+    uses = db.IntField(required=True, default=0)
+    height_min = db.IntField(required=True)
+    height_max = db.IntField(required=True)
+    last_user_email = db.EmailField(required=True)
+    key_number = db.StringField(required=True)
