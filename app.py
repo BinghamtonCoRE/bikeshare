a320e7082fbb34b998b509700fddcc2c1aead034
Joseph Palazzolo <jpalazz2@Josephs-MacBook-Pro-2.local>
2016-03-30 21:53:27 -0400

    App factory

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from bikeshare_app import app
+from bikeshare_app import create_app
 # Dont call app.run(). Uwsgi will do this for us
 
 if __name__ == "__main__":
+    app = create_app()
     app.run()
--- bikeshare_app/__init__.py
+++ bikeshare_app/__init__.py
@@ -4,12 +4,18 @@ from flask import Flask
 from flask.ext.mongoengine import MongoEngine
 from flask.ext.bower import Bower
 
-app = Flask(__name__)
-Bower(app)
-app.config['MONGODB_SETTINGS'] = {
-    'db': 'bikeshare'
-}
-app.debug = True
-db = MongoEngine(app)
+db = MongoEngine()
+
+def create_app(**config_overrides):
+    """Function to generate the app, this way we can change settings before
+    creating it"""
+    app = Flask(__name__)
+    Bower(app)
+    app.config['MONGODB_SETTINGS'] = {
+        'db': 'bikeshare'
+    }
+    app.config.update(config_overrides)
+    db.init_app(app)
+    return app
 
 import bikeshare_app.views
