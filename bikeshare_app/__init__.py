d134d260e2b5949884cd9ff2c1025424f217af4b
Taylor <lxmachina@gmail.com>
2016-03-31 20:58:59 -0400

    Admin authentication added

--- bikeshare_app/__init__.py
+++ bikeshare_app/__init__.py
@@ -1,13 +1,18 @@
 """Initialize the bikeshare app"""
-#pylint: disable=no-name-in-module,import-error,invalid-name,wrong-import-position
+# pylint: disable=no-name-in-module,import-error,invalid-name,wrong-import-position
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
 from flask.ext.bower import Bower
+import flask.ext.login as flask_login
 from bikeshare_app.config import configure_app
+from os import getenv
 
 app = Flask(__name__)
+app.secret_key = getenv("BIKESHARE_APP_SECRET")
 configure_app(app)
 Bower(app)
 db = MongoEngine(app)
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
 
 import bikeshare_app.views
